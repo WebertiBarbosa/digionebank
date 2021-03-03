6d95816cf73746218656664c9b6da0a3d6bb92e0
weberticf@gmail.com <weberticf@gmail.com>
2021-03-02 22:36:42 -0300

    primeira classe

--- src/main/kotlin/Pessoa.kt
+++ src/main/kotlin/Pessoa.kt
@@ -1,2 +1,12 @@
 class Pessoa {
+    var nome: String = "Weberti"
+    var cpf: String = "123.123.123-89"
+}
+
+fun main(){
+    val weberti = Pessoa()
+
+    println(weberti.nome)
+    println(weberti.cpf)
+
 }
